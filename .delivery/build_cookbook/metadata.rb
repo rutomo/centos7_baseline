4cf12e3516e0c86dd39bffd92188293776ae21e4
Rinaldi Utomo <github@rinaldiutomo.com>
2018-08-14 21:31:43 -0400

    Removed .delivery files

--- .delivery/build_cookbook/metadata.rb
+++
@@ -1,8 +0,0 @@
-name 'build_cookbook'
-maintainer 'Rinaldi Utomo'
-maintainer_email 'you@example.com'
-license 'gplv3'
-version '0.1.0'
-chef_version '>= 12.14' if respond_to?(:chef_version)
-
-depends 'delivery-truck'
