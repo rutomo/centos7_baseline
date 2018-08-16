d4c60d516b661852e953ea6ed91726ce45b3e853
Rinaldi Utomo <github@rinaldiutomo.com>
2018-08-15 21:21:10 -0400

    added packages to install

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,2 +1,7 @@
 #Filesystems to remove
 default['filesystems'] = ['cramfs','freevxfs','jffs2','hfs','hfsplus','squashfs','udf','vfat']
+
+default['packages_to_install'] = {
+    'rsyslog'=>'rsyslog',
+    'audit'=>'auditd'
+}
--- recipes/packages.rb
+++ recipes/packages.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook:: centos7-baseline
-# Recipe:: passwords
+# Recipe:: packages
 #
 # Copyright:: 2018,  Rinaldi Utomo
 #
@@ -15,17 +15,13 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-bash 'update_login_defs' do
-    code <<-EOH
-    sed -i 's/^PASS_MAX_DAYS.*/PASS_MAX_DAYS     60/pI' /etc/login.defs
-    sed -i 's/^PASS_MIN_DAYS.*/PASS_MIN_DAYS     7/pI' /etc/login.defs
-    echo "LOGIN_TIMEOUT 60" >> /etc/login.defs
-    echo "LOGIN_RETRIES 5" >> /etc/login.defs
-    echo "ENV_SUPATH PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin" >> /etc/login.defs
-    echo "ENV_PATH PATH=/usr/local/bin:/usr/bin:/bin" >> /etc/login.defs
-    chmod 444 /etc/login.defs
-    rm -f /usr/libexec/openssh/ssh-keysign
-    EOH
+node['packages_to_install'].each do |package_name,service_name|
+    package "#{package_name}" do
+        action :install        
+    end
+    service "#{service_name}" do
+        supports :status => false
+        action :start        
+    end    
 end
